1dc1eaf86732d81d318bb241db8d7a1c06745745
jivko25 <jivkou@yahoo.com>
2022-01-31 21:38:44 +0200

    Implement delete account

--- app/src/app/settings-modal/app.settings-modal.ts
+++ app/src/app/settings-modal/app.settings-modal.ts
@@ -61,4 +61,21 @@ export class AppSettings {
       this.hide();
     });
   }
+
+  deleteAccount(){
+    const data = JSON.parse(JSON.stringify(localStorage.getItem('user')))
+    const token = JSON.parse(data).sessionToken;
+    const userId = JSON.parse(data).objectId;
+
+    const headers = { 
+      'X-Parse-Application-Id': '3cLdch7H4CJ4jp7boKabPSTEmmUmB2d7RqEx7a0x', 
+      'X-Parse-REST-API-Key': 'UnO20LgpL4F0uS1Ahjpkuv7jxsol72xo3exjkP04', 
+      'X-Parse-Revocable-Session' : '1', 
+      'Content-Type' : 'application/json',
+      'X-Parse-Session-Token' : token };
+    this.http.delete<any>(`https://parseapi.back4app.com/users/${userId}`, { headers : headers }).subscribe(newData => {
+      this.hide();
+      localStorage.removeItem('user');
+    });
+  }
 }
